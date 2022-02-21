ede55f0d2f646a0be62d11cbd7ff7cdad49fc984
merlypops <62821275+merlypops@users.noreply.github.com>
2022-02-20 20:29:38 -0800

    upload analytics service files

---
+++ an-serv/analytics.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from pandas import Series, DataFrame
+import numpy as np
+import pymongo
+from pymongo import MongoClient
+
+client=MongoClient()
+client
