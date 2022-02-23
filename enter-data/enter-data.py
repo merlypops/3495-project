954ba8f0b8cf627a05dc64d5676b15d4f0941df8
merlypops <62821275+merlypops@users.noreply.github.com>
2022-02-22 18:40:28 -0800

    Update enter-data.py

--- enter-data/enter-data.py
+++ enter-data/enter-data.py
@@ -1,3 +1,7 @@
+import pyodbc
+import pandas as pd
+import mysql.connector
+
 n = int(input("how many values will you provide (integer): "))
 
 list1= []
@@ -6,3 +10,13 @@ for i in range(n):
     list1.append(value)
 
 print("list values are: ",list1)
+
+conn = mysql.connector.connect(host='localhost',
+                              database='docker',
+                              user='docker',
+                              password='docker')
+
+mysql_insert=""" INSERT INTO docker (values) VALUES (%s) """
+
+cursor=conn.cursor()
+cursor.execute(mysql_insert, list1)
